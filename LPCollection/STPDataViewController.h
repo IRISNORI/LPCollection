dc864f986dbe77a972c361e1b395cee0834ffb8e
Muramoto <Muramoto@Muramoto-iMac.local>
2013-12-03 21:55:54 +0900

    Initial Commit

---
+++ LPCollection/STPDataViewController.h
@@ -0,0 +1,16 @@
+//
+//  STPDataViewController.h
+//  LPCollection
+//
+//  Created by Muramoto on 2013/12/03.
+//  Copyright (c) 2013年 Norikazu Muramoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPDataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
---
+++ LPCollection/STPRootViewController.h
@@ -0,0 +1,15 @@
+//
+//  STPRootViewController.h
+//  LPCollection
+//
+//  Created by Muramoto on 2013/12/03.
+//  Copyright (c) 2013年 Norikazu Muramoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STPRootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
